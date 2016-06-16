8206fdc3259228b301981f68c0dad7b486e77094
sheafk <sheafureyking@gmail.com>
2016-06-16 15:18:41 -0400

    Done.

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -4,6 +4,13 @@
 
 @interface FISCardSwitcherViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *topLeftLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *middleLabel;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *bottomRightLabel;
+
 @end
 
 @implementation FISCardSwitcherViewController
@@ -21,5 +28,39 @@
     self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️"
                                                 rank:@"10"];
 }
+
+
+- (IBAction)threeOfSpadesClicked:(id)sender {
+    
+    self.topLeftLabel.text = @"♠️3";
+    self.middleLabel.text = @"♠️3";
+    self.bottomRightLabel.text = @"♠️3";
+    
+    
+    NSLog(@"3 of Spades was clicked");
+}
+
+- (IBAction)fourOfClubsClicked:(id)sender {
+    
+    self.topLeftLabel.text = @"♣️4";
+    self.middleLabel.text = @"♣️4";
+    self.bottomRightLabel.text = @"♣️4";
+}
+
+- (IBAction)eightOfDiamondsClicked:(id)sender {
+    
+    self.topLeftLabel.text = @"♦️8";
+    self.middleLabel.text = @"♦️8";
+    self.bottomRightLabel.text = @"♦️8";
+}
+
+- (IBAction)tenOfHeartsClicked:(id)sender {
+    
+    self.topLeftLabel.text = @"♥️10";
+    self.middleLabel.text = @"♥️10";
+    self.bottomRightLabel.text = @"♥️10";
+}
+
+
 
 @end
